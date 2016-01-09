2c76f97b579841eef118498803bfe428ea20d0c6
Nathan Worsham <nworsham@gmail.com>
2016-01-09 12:38:54 -0700

    Class Complete

---
+++ week8/whiskasmodel.py
@@ -0,0 +1,8 @@
+!#/usr/local/bin/python
+"""
+The Simplified Whiskas Model Python Formulation for the PuLP Modeller
+
+Authors: Antony Phillips, Dr Stuart Mitchell  2007
+"""
+# Import PuLP modeler functions
+from pulp import *
